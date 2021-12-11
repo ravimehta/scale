c8de75c7e929df4c6194910e022eebf9bf53470e
ravimehta <ravi@ravi-mehta.com>
2021-12-11 12:04:43 -0800

    Adding in custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,11 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      serif: ['Playfair Display', 'serif']
+    }
+  }
 }
